a5bb8305969771adbafa95d47d8e214d016dbea1
KrystianKaminski <krystiankaminski07@gmail.com>
2019-03-22 15:32:05 +0100

    Example of conditional rendering

--- src/ValidationComponent.js
+++ src/ValidationComponent.js
@@ -1,9 +1,21 @@
 import React from 'react'
 
 const ValidationComponent = (props) => {
+
+    let validation;
+
+    if (props.textLength < 5) {
+        validation = 'text too short'
+    } else if (props.textLength >= 5 && props.textLength <= 15) {
+        validation = 'text is ok'
+    } else if (props.textLength > 15) {
+        validation = 'text too long'
+    }
+
     return (
         <div>
             Length of the text in input field: {props.textLength}
+            <div>{validation}</div>
         </div>
     )
 }
